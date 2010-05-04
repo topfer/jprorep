0f62fd871dbf7c57418b899f163c0dc9b4b06549
thomas.fazekas <thomas.fazekas@14e84708-50d6-1587-599a-c615a35b04db>
2010-05-04 14:42:16 +0000

    class type selection box included in DIV containing edit form support for Safari,Firefox,Chrome code refactoring

--- trunk/details.pl
+++ trunk/details.pl
@@ -4,23 +4,27 @@ use CGI qw(:standard escapeHTML);
 
 require "base.pl";
 
+my $ldapEntry = &getLDAPEntry(param("nodeDN"));
+
 print "Content-type: text/html\r\n\r\n";
 
 print "<html><head><link type='text/css' rel='stylesheet' href='css/main.css'/></head>";
-print "<body onload='top.details.document.forms.nodeForm.elements.updateJSTree.value=\"".param("updateJSTree")."\";top.selectLDAPObjType()'><table border='1' width='100%'>";
-print "<tr><th width='150px' align='right'><b>Name</b></th><th><b>Value</b></th></tr>";
+#print "<body onload='top.details.document.forms.nodeForm.elements.updateJSTree.value=\"".param("updateJSTree")."\";top.selectLDAPObjType()'>";
+#print "<body onload='top.details.document.forms.nodeForm.elements.updateJSTree.value=\"".param("updateJSTree")."\";top.selectLDAPEntryForm(\"".$ldapEntry->get_value("objectClass")."\")'>";
+print "<body onload='top.selectLDAPEntryForm(\"".$ldapEntry->get_value("objectClass")."\");top.updateJSTree(\"".param("updateJSTree")."\")'>";
+#print "<table border='1' width='100%'><tr><th width='150px' align='right'><b>Name</b></th><th><b>Value</b></th></tr>";
 
 my $actualPredicate = param("predicate");
 
 if ( $actualPredicate eq "view" || $actualPredicate eq "details") {
-    &generateViewForm(param("nodeDN"));
+    &generateViewForm($ldapEntry);
 } elsif ( $actualPredicate eq "create" ) {
-    &generateCreateForm(param("nodeDN"));
+    &generateCreateForm($ldapEntry);
 } elsif ( $actualPredicate eq "edit" ) {
-    &generateEditForm(param("nodeDN"));
+    &generateEditForm($ldapEntry);
 }
 
-print "</table>";
+#print "</table>";
 print "</body></html>";
 
 
