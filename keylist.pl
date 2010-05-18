2094e07fdb60cd5b5c0d72c74cc70e80714b4cef
thomas.fazekas <thomas.fazekas@14e84708-50d6-1587-599a-c615a35b04db>
2010-05-18 20:41:43 +0000

    Export file name file Property descriptions as comments in exports

--- keylist.pl
+++ keylist.pl
@@ -4,7 +4,7 @@ use CGI qw(:standard escapeHTML);
 
 require "base.pl";
 
-#open(CGILOG, ">> /tmp/cgi.log");
+open(CGILOG, ">> /tmp/cgi.log");
 
 my $ldap = Net::LDAP->new ("localhost", port => 389, version => 3 );
 
@@ -16,14 +16,17 @@ if ( ! defined $currentBase || $currentBase eq "" || $currentBase eq "0" ) {
     $currentBase = $rootDN;
 }
 
-my $msg = $ldap->search(base => $currentBase, scope => one, filter => "(|(objectclass=propertyObject)(objectclass=alias))", attrs => '*');
+#my $msg = $ldap->search(base => $currentBase, scope => one, filter => "(|(objectclass=propertyObject)(objectclass=alias))", attrs => '*');
 
 $currentBase = substr($currentBase,0,rindex($currentBase, $rootDN) - 1);
 my $counter = 10;
 my $nextNodeStart = 0;
 
+my $keySearchFilter;
+
 sub getContainerLevelKeys {
-    my $keyList = $ldap->search(base => $_[0], scope => one, filter => "(|(objectclass=propertyObject)(objectclass=alias))", attrs => '*');
+    my $keyList = $ldap->search(base => $_[0], scope => one, filter => $keySearchFilter, attrs => '*');
+    #print CGILOG "Container DN : $_[0]\n";
     return $keyList;
 }
 
@@ -32,6 +35,8 @@ sub generateInheritedKeys {
     $prefix = $_[0];
     $delimiter = $_[1];
     $postfix = $_[2];
+    $commentprefix = $_[3];
+    $commentpostfix = $_[4];
 
     my ($keyName, $keyValue);
 
@@ -56,8 +61,13 @@ sub generateInheritedKeys {
             if ( $entry->get_value("objectclass") eq "alias" ) {
                 $entry = getLDAPEntry($entry->get_value("aliasedObjectName"));
             }
+            
+            if ( param("includePropertyComment") eq "1" ) {
+                print "\n".$commentprefix.$entry->get_value("description").$commentpostfix."\n";
+            }
+
             $keyName = $keyPrefix.$entry->get_value("cn");
-            print $prefix.$keyName.$delimiter.$entry->get_value("keyValue")."&nbsp;".$postfix;
+            print $prefix.$keyName.$delimiter.$entry->get_value("keyValue").$postfix;
         }
 
         $nextNodeStart = index($currentBase, ',', $nextNodeStart + 1) + 1;
@@ -65,11 +75,20 @@ sub generateInheritedKeys {
     } until  ( $nextNodeStart == 0  || $counter < 0);
 }
 
+if ( param("dereferenceLinks") eq "1" ) {
+    $keySearchFilter = "(|(objectclass=propertyObject)(objectclass=alias))";
+} else {
+    $keySearchFilter = "(objectclass=propertyObject)";
+}
+
 if ( param("predicate") eq "export" ) {
     if ( param("exportType") eq "prop" ) {
-        print "Content-Type:application/x-download\r\n\r\n";  
+        my $myCurrTime = &printCurrTime;
+        #print "Content-Type:application/x-download\r\n\r\n";
+        print "Content-Disposition:attachment;filename=settings_".$myCurrTime.".properties\r\n\r\n";  
+        #print "Content-Type:application/x-download\r\n\r\n";  
         #print "Content-Disposition:attachment;filename=toto.txt\r\n\r\n";  
-        &generateInheritedKeys("","=","\n");
+        &generateInheritedKeys("","=","\n","#","");
     } elsif ( param("exportType") eq "html" ) { 
         #print CGILOG "exportType : html\n";
         print "Content-type: text/html\r\n\r\n";
@@ -77,7 +96,7 @@ if ( param("predicate") eq "export" ) {
         print "<table border='1' width='100%'>";
         print "<tr><th width='150px' align='right'><b>Name</b></th><th><b>Value</b></th></tr>";
 
-        &generateInheritedKeys("<tr><td>","</td><td>","</td></tr>");
+        &generateInheritedKeys("<tr><td>","</td><td>","</td></tr>","<!--","-->");
 
         print "</table></body></html>\n";
     }
