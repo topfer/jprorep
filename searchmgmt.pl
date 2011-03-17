ef54ddebe5eec851711d2cc7a29b0d853652ac60
thomas.fazekas <thomas.fazekas@14e84708-50d6-1587-599a-c615a35b04db>
2011-03-17 07:24:09 +0000

    Additional tab to display searches More functionality in listing searches

--- searchmgmt.pl
+++ searchmgmt.pl
@@ -1,54 +1,80 @@
 #!/usr/bin/perl -w
 
 use DBI;
+use Net::LDAP;
 use Switch;
 use URI::Escape;
 use CGI qw(:standard escapeHTML);
 
-sub listSearches() {
+require "base.pl";
 
-    # Connect to the database.
-    my $dbh = DBI->connect("DBI:mysql:database=tst;host=192.168.56.101;port=3306",
-                         "arcore", "",
-                         {'RaiseError' => 1});
+open(CGILOG, ">> /tmp/cgi.log");
+
+sub listSearches() {
 
     # Now retrieve data from the table.
     my $sth = $dbh->prepare("SELECT * FROM searches");
     $sth->execute();
-    while (my $ref = $sth->fetchrow_hashref()) {
-        #print CGILOG logtime()."Found a row: id = ".$ref->{"sid"}."name = ".$ref->{"name"}."\n";
-        print "<tr><td>".$ref->{"sid"}."</td><td>".$ref->{"name"}."</td></tr>\n";
-    }
-    $sth->finish();
 
-    # Disconnect from the database.
-    $dbh->disconnect();
+    while (my $ref = $sth->fetchrow_hashref()) {
+        #print CGILOG logtime()."Found a row: id = ".$ref->{"id"}."name = ".$ref->{"srcname"}."\n";
+        print "<tr><td>".$ref->{"id"}.
+               "</td><td>".$ref->{"srcname"}.
+               "</td><td>".$ref->{"created"}.
+               "</td></tr>\n";
+    }
+
+    $sth->finish();
 }
 
 ################################################################################
 # main function
 ################################################################################
 
+# Connect to the database.
+$dbh = DBI->connect("DBI:mysql:database=arcoremgmt;host=192.168.56.101;port=3306",
+                       "arcore_user", "",
+                       {'RaiseError' => 1});
+
+print CGILOG logtime()."Predicate : ".param("predicate")."\n";
+
 switch ( param("predicate") ) {
 
+    case "list" {
+        print "Content-type: text/html\r\n\r\n";
+        print "<html><body>\n";
+        print "<table border='1' width='100%'>";
+        print "<tr><th width='150px' align='right'><b>ID</b></th><th><b>Name</b></th><th><b>Created</b></th></tr>";
+        listSearches();
+        print "</table></body></html>\n";        
+    }
     case "save" {
-        # Connect to the database.
-        my $dbh = DBI->connect("DBI:mysql:database=tst;host=192.168.56.101;port=3306",
-                               "arcore", "",
-                               {'RaiseError' => 1});
-        my $sth = $dbh->prepare("INSERT INTO searches SET NAME = '".param("exportType")."'");
+        
+        $testStr = "INSERT INTO searches \
+                                (srcname, settingsinheritance, upward_inheritance, downward_inheritance, prefix_keys_separator) \
+                                VALUES (".
+                                "'".param("actionPrameter")."',".
+                                param("enableSettingsInheritance").",".
+                                param("upwardInheritance").",".
+                                param("downwardInheritance").",".
+                                "'".param("prefixKeysSeparator")."')";
+
+        #print CGILOG logtime()."SQL : ".$testStr."\n";
+
+        my $sth = $dbh->prepare($testStr);
         $sth->execute();
 
         $sth->finish();
 
-        # Disconnect from the database.
-        $dbh->disconnect();
-
         print "Content-type: text/html\r\n\r\n";
         print "<html><body>\n";
         print "<table border='1' width='100%'>";
-        print "<tr><th width='150px' align='right'><b>Name</b></th><th><b>Value</b></th></tr>";
+        print "<tr><th width='150px' align='right'><b>ID</b></th><th><b>Name</b></th><th><b>Created</b></th></tr>";
         listSearches();
         print "</table></body></html>\n";
     }
 }
+
+# Disconnect from the database.
+$dbh->disconnect();
+
--- sql/searches.sql
+++ sql/searches.sql
@@ -1,3 +1,24 @@
-create database tst;
-create table searches (sid int(4) auto_increment, name varchar(32) not null, primary key sid);
-grant all on tst.* to arcore@'%';
+DROP DATABASE IF EXISTS arcoremgmt;
+CREATE DATABASE arcoremgmt;
+
+USE arcoremgmt;
+
+CREATE TABLE searches (
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       srcname VARCHAR(32) NOT NULL,
+       created TIMESTAMP DEFAULT NOW(),  
+       settingsinheritance BOOL,
+       upward_inheritance INT(2) NOT NULL,
+       downward_inheritance INT(2) NOT NULL,
+       settings_overwrite BOOL,
+       include_container_comment BOOL,
+       include_property_comment BOOL,
+       dereference_links BOOL,
+       prefix_keys BOOL,
+       prefix_keys_separator CHAR NOT NULL
+);
+
+GRANT ALL ON arcoremgmt.* TO arcore_user@'%';
+
+FLUSH PRIVILEGES;
+
